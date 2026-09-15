a23ce07e68b8b4938432b75839bf2c98442ce2df
Erik Serrano <31600622+axiomcura@users.noreply.github.com>
2026-09-15 11:56:56 +0200

    Change base path in Vite config
    Update base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded correctly on GitHub Pages
+  base: '/awesome-cytodata/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
